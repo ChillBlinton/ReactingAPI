074ef405095e58b18ccd9d0ecebe6b5ca82c2397
CB <wtbatson@gmail.com>
2022-03-21 10:12:42 -0500

    update close

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello from App Component!</h1>
-        </div>
-    );
-};
 
+    const [films, setFilms] = useState([]);
+    const [peep, setPeep] = useState([]);
+
+
+///fetchfilms
+    const getFilms = async () => {
+        const res = await fetch('https://ghibliapi.herokuapp.com/films')
+        const data = await res.json()
+        setFilms(data);
+    }
+   
+//peeps
+    const getPeep = async () => {
+        const res = await fetch('https://ghibliapi.herokuapp.com/people')
+        const data = await res.json()
+        setPeep(data);
+    }
+
+
+    //return maps
+
+    const Films = films.map(val => {
+        return<>
+            <div classname="col-md-3">
+
+
+            </div>
+        </>
+    }
+        )
+
+
+
+    //Create buttons
+
+    return (
+        <>
+            <button className='' onClick={() => getFilms()} >Load Films</button>
+            <button className='' onClick={() => getPeep()} >Load Peep</button>
+            
+        </>
+    )
+
+};
 export default App;
